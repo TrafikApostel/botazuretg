5cc3540d2aac8722bd29cfe105614da38b3164ab
TrafikApostel <56306440+TrafikApostel@users.noreply.github.com>
2023-06-09 16:48:45 +0300

    Create cheacker.py

---
+++ cheacker.py
@@ -0,0 +1,47 @@
+from time import sleep
+from tcp_latency import measure_latency
+import datetime
+import sqlite3
+conn = sqlite3.connect('servers.db',check_same_thread=False)
+cur = conn.cursor()
+cur.execute("""CREATE TABLE IF NOT EXISTS servers(
+   userid INT,
+   email TEXT,
+   password TEXT,
+   ip TEXT,
+   log TEXT,
+   pas Text,
+   time_add Text,
+   status Text,
+   status_use Text,
+   time_die Text
+   );
+""")
+conn.commit()
+def cheack(ip):
+    try:
+        r = measure_latency(host=ip, port=3389, runs=2, timeout=2.5)
+    except:
+        return False
+    if len(r)!=0:
+        return True
+    else:
+        return False
+while True:
+    records = cur.execute("SELECT * FROM servers")
+    all_orders = cur.fetchall()
+    for order in all_orders:
+        r = cheack(order[3])
+        if r == False:
+            sqlite_update_query = """Update servers set status = ? where email = ?"""
+            cur.executemany(sqlite_update_query, [('hz', order[1])])
+            conn.commit()
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(10000)
+    sleep(6400)
